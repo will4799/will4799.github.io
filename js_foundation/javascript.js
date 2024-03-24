9bae2847ff64738ff871ef15db1ac9efbe042e43
will4799 <49356078+will4799@users.noreply.github.com>
2024-03-25 08:49:58 +1300

    Delete js_foundation directory

--- js_foundation/javascript.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector("#btn");
-btn.onclick = () => alert("Hello World!");
-
-const btn1 = document.querySelector("#btn1");
-btn1.addEventListener("click", function (e) {
-    console.log(e.offsetX);
-})
